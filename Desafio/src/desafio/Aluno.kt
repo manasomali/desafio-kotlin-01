bd7a7d194fbb5058758c9072fd7c68d974125bd8
manasomali <matheus.marques_96@hotmail.com>
2020-09-25 16:30:20 -0300

    Parte A

---
+++ Desafio/src/desafio/Aluno.kt
@@ -0,0 +1,11 @@
+package desafio
+
+class Aluno(var nome:String, var sobrenome:String, var codigo:Int) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Aluno) return false
+        if (codigo != other.codigo) return false
+
+        return true
+    }
+}
