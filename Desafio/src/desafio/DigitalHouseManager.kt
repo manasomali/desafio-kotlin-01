e88d127da5e896836eb2276617ff69f642a277a3
manasomali <matheus.marques_96@hotmail.com>
2020-09-25 16:51:15 -0300

    Parte G

---
+++ Desafio/src/desafio/DigitalHouseManager.kt
@@ -0,0 +1,8 @@
+package desafio
+
+class DigitalHouseManager(var listaAlunos: MutableList<Aluno>,
+                          var listaProfessores: MutableList<Professor>,
+                          var listaCursos: MutableList<Curso>,
+                          var listaMatrículas: MutableList<Matricula>) {
+
+}
--- Desafio/src/desafio/Matricula.kt
+++ Desafio/src/desafio/Matricula.kt
@@ -5,5 +5,5 @@ import java.time.LocalDate
 class Matricula(var aluno: Aluno,
                 var curso: Curso,
                 var dataMatricula: LocalDate = LocalDate.now()) {
-    
+
 }
