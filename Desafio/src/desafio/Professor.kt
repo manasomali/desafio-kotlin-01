b874578ef142ea0fe2b5b9239f01fc4ebb1e52ac
manasomali <matheus.marques_96@hotmail.com>
2020-09-25 16:36:34 -0300

    Parte C

---
+++ Desafio/src/desafio/Professor.kt
@@ -0,0 +1,12 @@
+package desafio
+
+class Professor(var nome:String, var sobrenome:String, var tempoDeCasa:Int, var codigo:Int) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Aluno) return false
+        if (codigo != other.codigo) return false
+
+        return true
+    }
+
+}
