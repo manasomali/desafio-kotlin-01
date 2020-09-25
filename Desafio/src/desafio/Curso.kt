02fe5e5f8e4ebada78e580f22f1b03b75e54f415
manasomali <matheus.marques_96@hotmail.com>
2020-09-25 18:02:29 -0300

    Parte H

--- Desafio/src/desafio/Curso.kt
+++ Desafio/src/desafio/Curso.kt
@@ -1,16 +1,36 @@
 package desafio
 
-class Curso(var nome:String,
-            var codigo:Int,
+class Curso(var nome: String,
+            var codigo: Int,
             var professorTitular: ProfessorTitular,
             var professorAdjunto: ProfessorAdjunto,
-            var quantidadeMaxAlunos:Int,
-            var alunosMatriculados:MutableList<Aluno>) {
+            var quantidadeMaxAlunos: Int,
+            var alunosMatriculados: MutableList<Aluno>) {
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (other !is Aluno) return false
         if (codigo != other.codigo) return false
 
         return true
+    }
+    fun adicionarUmAluno(umAluno: Aluno): Boolean {
+        if(alunosMatriculados.equals(umAluno)) {
+            println("O código ${umAluno.codigo} já está sendo usado.")
+            return false
+        } else {
+            alunosMatriculados.add(umAluno)
+            println("Aluno ${umAluno.nome} matriculado com o código ${umAluno.codigo}.")
+            return true
+        }
+    }
+    fun excluirAluno(umAluno: Aluno): Boolean {
+        if(alunosMatriculados.equals(umAluno)) {
+            println("Aluno ${umAluno.nome} de código ${umAluno.codigo} excluido com sucesso.")
+            alunosMatriculados.remove(umAluno)
+            return true
+        } else {
+            println("Aluno ${umAluno.nome} de código ${umAluno.codigo} não existe.")
+            return false
+        }
     }
 }
